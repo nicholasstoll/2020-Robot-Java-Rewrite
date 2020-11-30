63ceca05cf6926a478c3311ad339f0f89ec75528
nicholasstoll <53276161+nicholasstoll@users.noreply.github.com>
2020-11-30 18:18:39 -0500

    Commiting for maximum send

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,15 +7,13 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import com.ctre.phoenix.*;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -25,68 +23,65 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
  * directory.
  */
 public class Robot extends TimedRobot {
-  WPI_TalonSRX frontLeftDriveTalon = new WPI_TalonSRX(0);
-  WPI_TalonSRX frontRightDriveTalon = new WPI_TalonSRX(1);
+    private final Joystick m_stick = new Joystick(0);
+    private final Timer m_timer = new Timer();
+    WPI_TalonSRX frontLeftDriveTalon = new WPI_TalonSRX(0);
+    WPI_TalonSRX frontRightDriveTalon = new WPI_TalonSRX(1);
+    WPI_VictorSPX rearLeftDriveVictor = new WPI_VictorSPX(2);
+    WPI_VictorSPX rearRightDriveVictor = new WPI_VictorSPX(3);
+    SpeedControllerGroup leftDriveGearbox = new SpeedControllerGroup(frontLeftDriveTalon, rearLeftDriveVictor);
+    SpeedControllerGroup rightDriveGearbox = new SpeedControllerGroup(frontRightDriveTalon, rearRightDriveVictor);
+    private final DifferentialDrive m_robotDrive = new DifferentialDrive(leftDriveGearbox, rightDriveGearbox);
 
-  WPI_VictorSPX rearLeftDriveVictor = new WPI_VictorSPX(2);
-  WPI_VictorSPX rearRightDriveVictor = new WPI_VictorSPX(3);
-
-  SpeedControllerGroup leftDriveGearbox = new SpeedControllerGroup(frontLeftDriveTalon, rearLeftDriveVictor);
-  SpeedControllerGroup rightDriveGearbox = new SpeedControllerGroup(frontRightDriveTalon, rearRightDriveVictor);
-
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(leftDriveGearbox, rightDriveGearbox);
-  private final Joystick m_stick = new Joystick(0);
-  private final Timer m_timer = new Timer();
-
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-  }
-
-  /**
-   * This function is run once each time the robot enters autonomous mode.
-   */
-  @Override
-  public void autonomousInit() {
-    m_timer.reset();
-    m_timer.start();
-  }
-
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    // Drive for 2 seconds
-    if (m_timer.get() < 2.0) {
-      m_robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
-    } else {
-      m_robotDrive.stopMotor(); // stop robot
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    @Override
+    public void robotInit() {
     }
-  }
 
-  /**
-   * This function is called once each time the robot enters teleoperated mode.
-   */
-  @Override
-  public void teleopInit() {
-  }
+    /**
+     * This function is run once each time the robot enters autonomous mode.
+     */
+    @Override
+    public void autonomousInit() {
+        m_timer.reset();
+        m_timer.start();
+    }
 
-  /**
-   * This function is called periodically during teleoperated mode.
-   */
-  @Override
-  public void teleopPeriodic() {
-    m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
-  }
+    /**
+     * This function is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic() {
+        // Drive for 2 seconds
+        if (m_timer.get() < 2.0) {
+            m_robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
+        } else {
+            m_robotDrive.stopMotor(); // stop robot
+        }
+    }
 
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-  }
+    /**
+     * This function is called once each time the robot enters teleoperated mode.
+     */
+    @Override
+    public void teleopInit() {
+    }
+
+    /**
+     * This function is called periodically during teleoperated mode.
+     */
+    @Override
+    public void teleopPeriodic() {
+        m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+    }
+
+    /**
+     * This function is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic() {
+    }
 }
---
+++ src/main/java/subsystems/Drive.java
@@ -0,0 +1,13 @@
+package subsystems;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Drive extends Command {
+    private final
+
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
